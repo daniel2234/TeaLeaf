5e93acd70c8c70b7d4462cd66c6fe5d5236d8ec3
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-09 09:55:33 -0400

    added the last name input

--- 2_Variables/name.rb
+++ 2_Variables/name.rb
@@ -8,3 +8,14 @@ puts "Hello " + name
 10.times do
 	puts name
 end
+
+#solution 4 Modify name.rb again so that it first asks the user for their
+# first name, saves it into a variable,
+# and then does the same for the last name.
+# Then outputs their full name all at once.
+
+puts "what is your first name?"
+first_name = gets.chomp
+puts "what is your last name?"
+last_name = gets.chomp
+puts "Hello " + first_name + " " + last_name
